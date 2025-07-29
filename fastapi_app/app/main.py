e7546147aba5191289cb267247881584ce256bf3
Vyacheslav Laguta <chechelofka@gmail.com>
2025-07-29 21:17:07 +0300

    Временные изменения

--- fastapi_app/app/main.py
+++ fastapi_app/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 @app.get("/health")
 async def health_check():
-    return {"status": True}
+    return {"status": True, "services": "Api"}
 
 DocumentFolder = "app/documents/images"
 os.makedirs(DocumentFolder, exist_ok=True)
