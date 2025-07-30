a9efafca40479683f222b6c45ff16e12edc96057
Vyacheslav Laguta <chechelofka@gmail.com>
2025-07-30 18:53:58 +0300

    3

--- fastapi_app/app/main.py
+++ fastapi_app/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 @app.get("/test_pipeline")
 async def health_check():
-    return {"status": True, "version": "v6"}
+    return {"status": True, "version": "v7"}
 
 
 DocumentFolder = "app/documents/images"
